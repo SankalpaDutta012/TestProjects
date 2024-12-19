901afe62e6b403e4b6c0b6d9de9bf23f2b77ff9c
Sankalpa Dutta <124279362+SankalpaDutta012@users.noreply.github.com>
2024-12-19 17:43:48 +0530

    Update qr_test.js

--- qr_test.js
+++ qr_test.js
@@ -9,3 +9,37 @@ QRCode.toFile(outputPath, url, function (err) {
   if (err) throw err;
   console.log('QR code saved to ' + outputPath);
 });
+
+
+
+const QRCode = require('qrcode');
+const Jimp = require('jimp');
+
+const url = 'https://www.youtube.com/watch?v=56_rgF058QI';
+
+const options = {
+  errorCorrectionLevel: 'H',
+  margin: 3,
+  color: {
+    dark: '#FF0000', // Red color
+    light: '#FFFF00' // Yellow color
+  }
+};
+
+
+
+# npm install qrcode jimp
+
+// Generate QR code
+QRCode.toBuffer(url, options, (err, buffer) => {
+  if (err) throw err;
+
+  // Save the image
+  Jimp.read(buffer, (err, image) => {
+    if (err) throw err;
+    image.write('FG_2_YT_2.png', (err) => {
+      if (err) throw err;
+      console.log('QR code saved to FG_2_YT_2.png');
+    });
+  });
+});
